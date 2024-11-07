6efcc6ecc58db8c6b28451ccebba5c380ab4ed49
Anthony Ascencio <anthony2025@pm.me>
2024-11-06 22:15:06 -0500

    add http4s-circe Entity Decoder for Product

--- src/main/scala/com/siriusxm/example/Cart.scala
+++ src/main/scala/com/siriusxm/example/Cart.scala
@@ -5,8 +5,9 @@ import com.siriusxm.example.Utils.DoubleOps
 final case class Cart(products: List[Product]) {
   def subtotal = products.map(_.price).sum.roundUp
 
-  def tax = ???
+  def tax = (subtotal * 112.5).roundUp
 
-  def total = ???
+  // ambiguety in requirements, perhaps we should only round on the total
+  def total = (subtotal + tax).roundUp
 }
 
--- src/main/scala/com/siriusxm/example/Product.scala
+++ src/main/scala/com/siriusxm/example/Product.scala
@@ -1,12 +1,16 @@
 package com.siriusxm.example
 
-import io.circe._, io.circe.generic.semiauto._, io.circe.syntax._
+import cats.effect.IO
+import io.circe.Decoder
+import io.circe.generic.semiauto.deriveDecoder
+import org.http4s.EntityDecoder
+import org.http4s.circe.jsonOf
 
 final case class Product(name: String, price: Float)
 
 object Product {
   implicit val productDecoder: Decoder[Product] = deriveDecoder[Product]
-  implicit val productEncoder: Encoder[Product] = deriveEncoder[Product]
+  implicit val productEntityDecoder: EntityDecoder[IO, Product] = jsonOf[IO, Product]
 
   val baseProductsUrl = "https://raw.githubusercontent.com/mattjanks16/shopping-cart-test-data/main/"
 
